5f06f487f425c0dd86caa9101a0da57ea98d8934
Abdullahi Alao <ridwanoladokun005@gmail.com>
2025-07-06 12:40:27 -0400

    project updated

--- src/containers/work/workData.js
+++ src/containers/work/workData.js
@@ -1,6 +1,30 @@
 const workData = [
   {
     id: "1",
+    title: "React E-commerce Website",
+    description: [
+      "A fully responsive online clothing store built using React. It allows users to browse, filter, and purchase fashion items for men, women, and kids.",
+      "Integrated React Context API to manage shopping cart functionality, and used reusable components to structure the entire shopping experience.",
+    ],
+    imageUrls: [
+      "/images/Ecommerce/1.png",
+      "/images/Ecommerce/2.png",
+      "/images/Ecommerce/3.png",
+      "/images/Ecommerce/4.png",
+      "/images/Ecommerce/5.png",
+      "/images/Ecommerce/6.png",
+    ],
+    projectLink: "https://github.com/Ola157/shoppers",
+    tags: [
+      "React",
+      "JavaScript",
+      "Context API",
+      "E-commerce",
+      "Responsive Design",
+    ],
+  },
+  {
+    id: "2",
     title: "Quiz Game App",
     description: [
       "A responsive web quiz app built with React and Context API to manage global state. It features dynamic questions, a countdown timer, and real-time scoring.",
@@ -15,7 +39,7 @@ const workData = [
     tags: ["React", "Context API", "JavaScript", "Responsive UI", "Frontend"],
   },
   {
-    id: "2",
+    id: "3",
     title: "Meal Planner Landing Page",
     description: [
       "A visually appealing landing page designed to promote a meal planning service. Built with HTML, CSS, and basic JavaScript for interactivity.",
